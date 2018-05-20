f519cbdc592cdd321dd6f300c78722329317dcd6
Joona Viertola <contact@joonaviertola.com>
2018-05-20 15:21:15 +0300

    Fixed server paths to only one react app.

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
-const hostname = process.env.HOST || 'localhost';
-const publicPath = `http://${hostname}:${port}`;
-
 const app = express();
 const server = http.Server(app);
 
@@ -13,19 +11,7 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
-  const scriptPath = `${publicPath}/static/screen.js`;
-  res.render('default', {
-    title: 'Sausage Party - Screen',
-    viewScript: scriptPath
-  });
-});
-
-app.get('/player', (req, res) => {
-  const scriptPath = `${publicPath}/static/player.js`;
-  res.render('player', {
-    title: 'Sausage Party - Player',
-    viewScript: scriptPath
-  });
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 server.listen(port, () => {
